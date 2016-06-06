e1e0bc5fa3619dc77e3736d35db02342a853c29a
Shane <shane@cd2solutions.co.uk>
2016-06-06 09:15:32 +0100

    this

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
+    r301 /.*/, Proc.new {|path, rack_env| "http://www.#{rack_env['SERVER_NAME']}#{path}" }, :if => Proc.new {|rack_env| rack_env['SERVER_NAME'] !~ /www\./i}
+  end
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-
+  get '/index.html', to: redirect('/')
+  get '/reservations.html', to: redirect('/book-online')
+  get '/bar.html', to: redirect('/restaurant')
+  get '/gallery.html', to: redirect('/gallery')
+  get '/heroes.html', to: redirect('/restaurant')
   resources :testimonials
   root 'pages#home'
 
@@ -42,10 +46,6 @@ Rails.application.routes.draw do
   resources :enquiries, only: [:create, :new]
   resources :pages, path: '', only: :show
 
-  get '/index.html', to: redirect('/')
-  get '/reservations.html', to: redirect('/book-online')
-  get '/bar.html', to: redirect('/restaurant')
-  get '/gallery.html', to: redirect('/gallery')
-  get '/heroes.html', to: redirect('/restaurant')
+
 
 end
